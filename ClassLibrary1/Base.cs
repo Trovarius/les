cbdd04063c41ec66aac5e380e228412d0faa515e
Trovarius <tordek.coast@gmail.com>
2010-09-21 23:33:54 -0300

    criacao do projeto

---
+++ ClassLibrary1/Base.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    class Base
+    {
+        Dictionary<String, Column> colunas;
+    }
+}
---
+++ ClassLibrary1/Structs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public struct Column
+    {
+        Type tipo;
+        Int32 length;
+        String nome;
+        Boolean isNull;
+        Object value; //não decidio beleza
+
+        #region Propriedades
+
+        public Type Tipo
+        {
+            get { return tipo; }
+            set { tipo = value; }
+        }
+
+        public Int32 Length
+        {
+            get { return length; }
+            set { length = value; }
+        }
+
+        public String Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        #endregion
+
+    }
+}
